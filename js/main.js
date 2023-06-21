642aecb622e6593394e2e8a35e254e37c4840b8a
chinthaka <chinthakaweerasekara9@gmail.com>
2023-06-21 21:37:03 +0530

    new user creation form

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let userArr=[];
+
+class User{
+    constructor(fullName,email,password) {
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+    }
+ }
+
+/*User Management*/
+ const createUser=()=>{
+     let createdUser = new User('','','');
+     userArr.push(createdUser);
+ }
+ /*User Management*/
+const clearAndLoad=(element)=>{
+    //clear, toggle
+}
