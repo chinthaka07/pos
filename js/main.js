724bd42bfb91e1723a455e81dc2a2cbd79ebaac0
chinthaka <chinthakaweerasekara9@gmail.com>
2023-06-21 22:22:22 +0530

    user login

--- js/main.js
+++ js/main.js
@@ -15,8 +15,11 @@ class User{
      const email=$('#register-email').val();
      const password=$('#register-password').val();
 
+     //const hash = bcrypt.hashSync(password,bcrypt.genSaltSync(10));
+     //console.log(hash);
+
      let createdUser = new User(fullName,email,password);
-     let existUserData=userArr.find(e=>e.email=='');
+     let existUserData=userArr.find(e=>e.email===email);
      if (existUserData){
          alert('User email already exist!');
          return;
@@ -24,6 +27,26 @@ class User{
      userArr.push(createdUser);
      console.log(userArr);
  }
+
+ const login=()=>{
+     const email=$('#register-email').val();
+     const password=$('#register-password').val();
+
+     let existUserData=userArr.find(e=>e.email===email);
+
+     if (existUserData){
+         if (existUserData.password===password){
+             //login=> dashboard
+
+         } else {
+             alert('password is wrong');
+             return;
+         }
+     } else {
+         alert('User email not found');
+     }
+ }
+
  /*User Management*/
 const clearAndLoad=(element)=>{
     //clear, toggle
